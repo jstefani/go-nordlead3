7730e0e207d112f2d7a40470543793e37b1b82a4
Malacalypse <malacalypse@users.noreply.github.com>
2019-07-28 15:08:49 +0000

    Reorder patchmemory for logical grouping

--- patchmemory.go
+++ patchmemory.go
@@ -32,6 +32,159 @@ type PatchMemory struct {
 	slotPrograms    [4]*Program
 }
 
+func (memory *PatchMemory) ExportAllPerformances(filename string) error {
+	var refs []patchRef
+
+	for i, _ := range memory.performances {
+		refs = append(refs, patchRef{PerformanceT, MemoryT, i})
+	}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) ExportAllPrograms(filename string) error {
+	var refs []patchRef
+
+	for i, _ := range memory.programs {
+		refs = append(refs, patchRef{ProgramT, MemoryT, i})
+	}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) ExportPerformance(bank, location int, filename string) error {
+	refs := []patchRef{patchRef{PerformanceT, MemoryT, index(bank, location)}}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) ExportPerformanceBank(bank int, filename string) error {
+	var refs []patchRef
+
+	for i := 0; i < bankSize; i++ {
+		refs = append(refs, patchRef{PerformanceT, MemoryT, index(bank, i)})
+	}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) ExportProgram(bank, location int, filename string) error {
+	refs := []patchRef{patchRef{ProgramT, MemoryT, index(bank, location)}}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) ExportProgramBank(bank int, filename string) error {
+	var refs []patchRef
+
+	for i := 0; i < bankSize; i++ {
+		refs = append(refs, patchRef{ProgramT, MemoryT, index(bank, i)})
+	}
+	return memory.exportLocations(refs, filename)
+}
+
+func (memory *PatchMemory) Import(rawSysex []byte) error {
+	err := *new(error)
+	sysex, err := parseSysex(rawSysex)
+	if err != nil {
+		return err
+	}
+
+	_, err = sysex.valid()
+	if err != nil {
+		return err
+	}
+
+	switch sysex.messageType() {
+	case programFromMemory, programFromSlot:
+		memory.importProgram(sysex)
+	case performanceFromMemory, performanceFromSlot:
+		memory.importPerformance(sysex)
+	}
+
+	return nil
+}
+
+func (memory *PatchMemory) ImportFrom(input io.Reader) (numValid int, numInvalid int, err error) {
+	validFound, invalidFound := 0, 0
+	reader := bufio.NewReader(input)
+
+	// TODO: Refactor this as a scanner break function and scan the string elegantly
+	for {
+		// scan until we see an F0, we hit EOF, or an error occurs.
+		_, err := reader.ReadBytes(sysexStart)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return validFound, invalidFound, err
+			}
+		}
+
+		// Read the sysex header to see if it's data we care about
+		header, _ := reader.Peek(3)
+		header[1] = 0x00 // We don't care about the destination address
+
+		// 0x33 = Clavia, 0x00 = dest. addr blanked above, 0x09 = NL3 sysex model ID
+		if string(header) == string([]byte{0x33, 0x00, 0x09}) {
+			sysex, err := reader.ReadBytes(sysexEnd)
+			if err != nil {
+				return 0, 0, err
+			}
+
+			err = memory.Import(sysex)
+			if err == nil {
+				validFound++
+			} else {
+				invalidFound++
+			}
+		}
+	}
+	return validFound, invalidFound, nil
+}
+
+func (memory *PatchMemory) SprintPrograms(omitBlank bool) string {
+	var result []string
+	currBank := -1 // won't match any bank
+
+	result = append(result, "\n***** PROGRAMS ******\n")
+	for i, program := range memory.programs {
+		bank, location := bankloc(i)
+
+		if memory.initialized(patchRef{ProgramT, MemoryT, i}) || !omitBlank {
+			if bank != currBank {
+				bank_header := fmt.Sprintf("\n*** Bank %v ***\n", bank+1)
+				result = append(result, bank_header)
+				currBank = bank
+			}
+
+			result = append(result, fmt.Sprintf("   %3d : %s", location+1, program.Summary()))
+		}
+	}
+
+	return strings.Join(result, "\n")
+}
+
+func (memory *PatchMemory) SprintPerformances(omitBlank bool) string {
+	var result []string
+	currBank := -1 // won't match any bank
+
+	result = append(result, "\n***** PERFORMANCES ******\n")
+
+	for i, perf := range memory.performances {
+		bank, location := bankloc(i)
+
+		if memory.initialized(patchRef{PerformanceT, MemoryT, i}) || !omitBlank {
+			if bank != currBank {
+				bank_header := fmt.Sprintf("\n*** Bank %v ***\n", bank+1)
+				result = append(result, bank_header)
+				currBank = bank
+			}
+
+			result = append(result, fmt.Sprintf("   %3d : %s", location+1, perf.Summary()))
+		}
+	}
+
+	return strings.Join(result, "\n")
+}
+
+// Core internal behaviours
+
 func (memory *PatchMemory) clear(ref patchRef) {
 	if memory.initialized(ref) {
 		switch ref.PatchType {
@@ -87,52 +240,6 @@ func (memory *PatchMemory) exportLocations(refs []patchRef, filename string) err
 	return exportToFile(&exportdata, filename, false)
 }
 
-func (memory *PatchMemory) ExportAllPerformances(filename string) error {
-	var refs []patchRef
-
-	for i, _ := range memory.performances {
-		refs = append(refs, patchRef{PerformanceT, MemoryT, i})
-	}
-	return memory.exportLocations(refs, filename)
-}
-
-func (memory *PatchMemory) ExportAllPrograms(filename string) error {
-	var refs []patchRef
-
-	for i, _ := range memory.programs {
-		refs = append(refs, patchRef{ProgramT, MemoryT, i})
-	}
-	return memory.exportLocations(refs, filename)
-}
-
-func (memory *PatchMemory) ExportPerformance(bank, location int, filename string) error {
-	refs := []patchRef{patchRef{PerformanceT, MemoryT, index(bank, location)}}
-	return memory.exportLocations(refs, filename)
-}
-
-func (memory *PatchMemory) ExportPerformanceBank(bank int, filename string) error {
-	var refs []patchRef
-
-	for i := 0; i < bankSize; i++ {
-		refs = append(refs, patchRef{PerformanceT, MemoryT, index(bank, i)})
-	}
-	return memory.exportLocations(refs, filename)
-}
-
-func (memory *PatchMemory) ExportProgram(bank, location int, filename string) error {
-	refs := []patchRef{patchRef{ProgramT, MemoryT, index(bank, location)}}
-	return memory.exportLocations(refs, filename)
-}
-
-func (memory *PatchMemory) ExportProgramBank(bank int, filename string) error {
-	var refs []patchRef
-
-	for i := 0; i < bankSize; i++ {
-		refs = append(refs, patchRef{ProgramT, MemoryT, index(bank, i)})
-	}
-	return memory.exportLocations(refs, filename)
-}
-
 // Returns a generic patch interface, could be either a program or a performance.
 // It remains to the receiver to assert the patch to the necessary type.
 func (memory *PatchMemory) get(ref patchRef) (patch, error) {
@@ -175,113 +282,30 @@ func (memory *PatchMemory) set(ref patchRef, patch patch) error {
 	return err
 }
 
-func (memory *PatchMemory) Import(rawSysex []byte) error {
-	err := *new(error)
-	sysex, err := parseSysex(rawSysex)
-	if err != nil {
-		return err
+// Panics if locations are invalid
+func (memory *PatchMemory) swap(src patchRef, dest patchRef) error {
+	if src.PatchType != dest.PatchType {
+		return ErrXferTypeMismatch
+	}
+	if src.source != dest.source {
+		return ErrXferTypeMismatch // Don't support swapping to/from a slot, should be a copy or move.
 	}
 
-	_, err = sysex.valid()
-	if err != nil {
-		return err
+	switch src.PatchType {
+	case PerformanceT:
+		srcPtr := memory.perfPtr(src)
+		destPtr := memory.perfPtr(dest)
+		temp := *destPtr
+		*destPtr = *srcPtr
+		*srcPtr = temp
+	case ProgramT:
+		srcPtr := memory.progPtr(src)
+		destPtr := memory.progPtr(dest)
+		temp := *destPtr
+		*destPtr = *srcPtr
+		*srcPtr = temp
 	}
-
-	switch sysex.messageType() {
-	case programFromMemory, programFromSlot:
-		memory.loadProgramFromSysex(sysex)
-	case performanceFromMemory, performanceFromSlot:
-		memory.loadPerformanceFromSysex(sysex)
-	}
-
 	return nil
-}
-
-func (memory *PatchMemory) ImportFrom(input io.Reader) (numValid int, numInvalid int, err error) {
-	validFound, invalidFound := 0, 0
-	reader := bufio.NewReader(input)
-
-	// TODO: Refactor this as a scanner break function and scan the string elegantly
-	for {
-		// scan until we see an F0, we hit EOF, or an error occurs.
-		_, err := reader.ReadBytes(sysexStart)
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				return validFound, invalidFound, err
-			}
-		}
-
-		// Read the sysex header to see if it's data we care about
-		header, _ := reader.Peek(3)
-		header[1] = 0x00 // We don't care about the destination address
-
-		// 0x33 = Clavia, 0x00 = dest. addr blanked above, 0x09 = NL3 sysex model ID
-		if string(header) == string([]byte{0x33, 0x00, 0x09}) {
-			sysex, err := reader.ReadBytes(sysexEnd)
-			if err != nil {
-				return 0, 0, err
-			}
-
-			err = memory.Import(sysex)
-			if err == nil {
-				validFound++
-			} else {
-				invalidFound++
-			}
-		}
-	}
-	return validFound, invalidFound, nil
-}
-
-func (memory *PatchMemory) loadPerformanceFromSysex(s *sysex) error {
-	var ref patchRef
-
-	performanceData, err := newPerformanceFromBitstream(s.decodedBitstream)
-	if err == nil {
-		performance := Performance{
-			name:     s.nameAsArray(),
-			category: s.category(),
-			version:  s.version(),
-			data:     performanceData,
-		}
-
-		if s.messageType() == performanceFromSlot {
-			ref = performanceSlotRef
-		} else {
-			ref = patchRef{PerformanceT, MemoryT, index(s.bank(), s.location())}
-		}
-		if existing, err := memory.get(ref); err == nil {
-			fmt.Printf("Overwriting %s (%q) with %q\n", ref.String(), existing.PrintableName(), s.printableName())
-		}
-		err = memory.set(ref, &performance)
-	}
-	return err
-}
-
-func (memory *PatchMemory) loadProgramFromSysex(s *sysex) error {
-	var ref patchRef
-
-	programData, err := newProgramFromBitstream(s.decodedBitstream)
-	if err == nil {
-		program := Program{
-			name:     s.nameAsArray(),
-			category: s.category(),
-			version:  s.version(),
-			data:     programData,
-		}
-		if s.messageType() == programFromSlot {
-			ref = patchRef{ProgramT, SlotT, s.bank()}
-		} else {
-			ref = patchRef{ProgramT, MemoryT, index(s.bank(), s.location())}
-		}
-		if existing, err := memory.get(ref); err == nil {
-			fmt.Printf("Overwriting %s (%q) with %q\n", ref.String(), existing.PrintableName(), s.printableName())
-		}
-		err = memory.set(ref, &program)
-	}
-	return err
 }
 
 // transfer can behave as a copy (mode is copyM) or a move (mode is moveM).
@@ -340,75 +364,55 @@ func (memory *PatchMemory) transfer(src []patchRef, dest patchRef, mode transfer
 	return err
 }
 
-// Panics if locations are invalid
-func (memory *PatchMemory) Swap(src patchRef, dest patchRef) error {
-	if src.PatchType != dest.PatchType {
-		return ErrXferTypeMismatch
-	}
-	if src.source != dest.source {
-		return ErrXferTypeMismatch // Don't support swapping to/from a slot, should be a copy or move.
-	}
+// helpers
 
-	switch src.PatchType {
-	case PerformanceT:
-		srcPtr := memory.perfPtr(src)
-		destPtr := memory.perfPtr(dest)
-		temp := *destPtr
-		*destPtr = *srcPtr
-		*srcPtr = temp
-	case ProgramT:
-		srcPtr := memory.progPtr(src)
-		destPtr := memory.progPtr(dest)
-		temp := *destPtr
-		*destPtr = *srcPtr
-		*srcPtr = temp
+func (memory *PatchMemory) importPerformance(s *sysex) error {
+	var ref patchRef
+
+	performanceData, err := newPerformanceFromBitstream(s.decodedBitstream)
+	if err == nil {
+		performance := Performance{
+			name:     s.nameAsArray(),
+			category: s.category(),
+			version:  s.version(),
+			data:     performanceData,
+		}
+
+		if s.messageType() == performanceFromSlot {
+			ref = performanceSlotRef
+		} else {
+			ref = patchRef{PerformanceT, MemoryT, index(s.bank(), s.location())}
+		}
+		if existing, err := memory.get(ref); err == nil {
+			fmt.Printf("Overwriting %s (%q) with %q\n", ref.String(), existing.PrintableName(), s.printableName())
+		}
+		err = memory.set(ref, &performance)
 	}
-	return nil
+	return err
 }
 
-func (memory *PatchMemory) SprintPrograms(omitBlank bool) string {
-	var result []string
-	currBank := -1 // won't match any bank
+func (memory *PatchMemory) importProgram(s *sysex) error {
+	var ref patchRef
 
-	result = append(result, "\n***** PROGRAMS ******\n")
-	for i, program := range memory.programs {
-		bank, location := bankloc(i)
-
-		if memory.initialized(patchRef{ProgramT, MemoryT, i}) || !omitBlank {
-			if bank != currBank {
-				bank_header := fmt.Sprintf("\n*** Bank %v ***\n", bank+1)
-				result = append(result, bank_header)
-				currBank = bank
-			}
-
-			result = append(result, fmt.Sprintf("   %3d : %s", location+1, program.Summary()))
+	programData, err := newProgramFromBitstream(s.decodedBitstream)
+	if err == nil {
+		program := Program{
+			name:     s.nameAsArray(),
+			category: s.category(),
+			version:  s.version(),
+			data:     programData,
 		}
-	}
-
-	return strings.Join(result, "\n")
-}
-
-func (memory *PatchMemory) SprintPerformances(omitBlank bool) string {
-	var result []string
-	currBank := -1 // won't match any bank
-
-	result = append(result, "\n***** PERFORMANCES ******\n")
-
-	for i, perf := range memory.performances {
-		bank, location := bankloc(i)
-
-		if memory.initialized(patchRef{PerformanceT, MemoryT, i}) || !omitBlank {
-			if bank != currBank {
-				bank_header := fmt.Sprintf("\n*** Bank %v ***\n", bank+1)
-				result = append(result, bank_header)
-				currBank = bank
-			}
-
-			result = append(result, fmt.Sprintf("   %3d : %s", location+1, perf.Summary()))
+		if s.messageType() == programFromSlot {
+			ref = patchRef{ProgramT, SlotT, s.bank()}
+		} else {
+			ref = patchRef{ProgramT, MemoryT, index(s.bank(), s.location())}
 		}
+		if existing, err := memory.get(ref); err == nil {
+			fmt.Printf("Overwriting %s (%q) with %q\n", ref.String(), existing.PrintableName(), s.printableName())
+		}
+		err = memory.set(ref, &program)
 	}
-
-	return strings.Join(result, "\n")
+	return err
 }
 
 func (memory *PatchMemory) initialized(ref patchRef) (result bool) {
